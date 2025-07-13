3d6a35159e257e4a3838215a9a556de5b615ccab
Miyashi <139875433+ShogoMiyashita@users.noreply.github.com>
2025-07-13 14:57:03 +0900

    Setup Xcode project and add dependencies

---
+++ PokemonSearch/PokemonSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSearchApp.swift
+//  PokemonSearch
+//
+//  Created by Shogo Miyashita on 2025/07/13.
+//
+
+import SwiftUI
+
+@main
+struct PokemonSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
